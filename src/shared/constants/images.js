832466c4ab28a90fc66ed7df997d58c480ad1992
Manbir <msinghus96@gmail.com>
2019-09-12 07:15:55 +0530

    Added fonts for vector icons

---
+++ src/shared/constants/images.js
@@ -0,0 +1,3 @@
+export const IMAGES = {
+  HEADER: require('../../assets/Header.png'),
+};
---
+++ src/shared/constants/text.js
@@ -0,0 +1,29 @@
+import {classes} from 'istanbul-lib-coverage';
+
+export const TEXT = {
+  MY_PROFILE: 'MY PROFILE',
+  CREATE_PROFILE: 'Create Profile',
+  CREATE_PROFILE_DESCRIPTION:
+    'Enter the information of the user you want to create the profile for.',
+  FIRST_NAME: 'First Name',
+  LAST_NAME: 'Last Name',
+  BIRTHDAY: 'Birthday',
+  ALLERGIES: 'Allergies',
+  NOTES: 'Notes',
+  CREATE_PROFILE: 'Create Profile',
+  UPDATE_PROFILE: 'Update Profile',
+  LEVEL: 'Level',
+  BIO: 'BIO',
+  CLASSES: 'Classes',
+  TEAMS: 'Teams',
+  CLICK_HERE: 'Click here',
+  VIEW_CLASS_LIST: 'to view our class listing.',
+  NO_PERMISSION_CLASS: "Looks like you aren't enrolled in any classes.",
+  BOTTOM_TAB: {
+    FEED: 'Feed',
+    CALENDAR: 'Calendar',
+    STUDIO: 'Studio',
+    MESSAGING: 'Messaging',
+    PROFILE: 'Profile',
+  },
+};
